a1c14e3080be1f650faa312e8a60864137497978
Siraj <sgsdbs@gmail.com>
2022-10-14 16:50:51 +0300

    Initial commit

---
+++ FolderSolution/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FolderSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ops = new string[] {"d1/","d2/","../","d21/","./"};
+            
+            Console.WriteLine(Solution(ops));
+        }
+
+        static byte Solution(string[] ops)
+        {
+            // для экономии памяти
+            // т.к ops.length <= 103
+            byte begin = 0;
+
+            for(byte i = 0; i < ops.Length; i++)
+            {
+                if (ops[i] == "./")
+                {
+                    continue;
+                }
+
+                if (ops[i] == "../" && begin > 0)
+                {
+                    begin--;
+                }
+                else
+                {
+                    begin++;
+                }
+            }
+            
+            return begin;
+        }
+    }
+}
